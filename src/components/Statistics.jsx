7f0a8d618fd3dad0bbfd257d2a85ec6976c8357a
ToporekDaniel <danieltoporek0@gmail.com>
2024-01-14 21:34:38 +0100

    mix stylowania

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -1,18 +1,50 @@
+import styled from 'styled-components';
+
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+const StatSection = styled.section`
+  background-color: ${getRandomColor};
+  padding: 10px;
+`;
+
+const StatList = styled.ul`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  grid-gap: 10px;
+`;
+
+const StatItem = styled.li`
+  background-color: ${getRandomColor};
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 20px;
+  color: white;
+`;
+
 export const Stats = ({ title, stats }) => {
   function createLi() {
     return stats.map(i => (
-      <li class="item">
-        <span class="label">{i.label}</span>
-        <span class="percentage">{i.percentage}</span>
-      </li>
+      <StatItem className="item">
+        <span className="label">{i.label}</span>
+        <span className="percentage">{i.percentage}</span>
+      </StatItem>
     ));
   }
 
   return (
-    <section class="statistics">
-      <h2 class="title">{title}</h2>
-
-      <ul class="stat-list">{createLi()}</ul>
-    </section>
+    <StatSection className="statistics">
+      <h2 style={{ alignItems: 'center', display: 'flex' }} className="title">
+        {title}
+      </h2>
+      <StatList className="stat-list">{createLi()}</StatList>
+    </StatSection>
   );
 };
