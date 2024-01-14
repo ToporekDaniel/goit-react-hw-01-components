2f649f4ad3a206ca175df70f5d8e4354923461fc
ToporekDaniel <danieltoporek0@gmail.com>
2024-01-14 20:01:16 +0100

    hw1 renders

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
 import './index.css';
+import data from './user.json';
+import { Profile } from 'components/App';
+
+const { username, tag, location, avatar, stats } = data;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Profile
+      username={username}
+      tag={tag}
+      location={location}
+      avatar={avatar}
+      stats={stats}
+    />
   </React.StrictMode>
 );
